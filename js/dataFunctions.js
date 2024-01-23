3cb83c13f9d18e8df6ec8404fc46c790466b0b1a
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-01-23 10:58:04 -0600

    Added 'return searchTerm' to getSearchTerm

--- js/dataFunctions.js
+++ js/dataFunctions.js
@@ -2,4 +2,5 @@ export const getSearchTerm = () => {
   const rawSearchTerm = document.getElementById("search").value.trim();
   const regex = /[ ]{2,}/gi; // Don't want 2 spaces in search term
   const searchTerm = rawSearchTerm.replaceAll(regex, " ");
+  return searchTerm;
 };
