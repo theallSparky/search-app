67a92f9250bc491fc554077c569f7e8b36b36675
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-01-25 19:17:12 -0600

    Made const requestData and retrieveSearchResults logic

--- js/dataFunctions.js
+++ js/dataFunctions.js
@@ -7,12 +7,19 @@ export const getSearchTerm = () => {
 
 export const retrieveSearchResults = async (searchTerm) => {
   const wikiSearchString = getWikiSearchString(searchTerm);
+  const wikiSearchResults = await requestData(wikiSearchString);
+  let resultArray = [];
+  if (wikiSearchResults.hasOwnProperty("query")) {
+    resultArray = processWikiResults(wikiSearchResults.query.pages);
+  }
+  return resultArray;
 };
 
 const getWikiSearchString = (searchTerm) => {
   const maxChars = getMaxChars();
   const rawSearchString = `https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=${searchTerm}&gsrlimit=20&prop=pageimages|extracts&exchars=${maxChars}&exintro&explaintext&exlimit=max&format=json&origin=*`;
   const searchString = eccodeURI(rawSearchString);
+  return searchString;
 };
 
 const getMaxChars = () => {
@@ -22,4 +29,34 @@ const getMaxChars = () => {
   if (width >= 414 && width < 1400) maxChars = 100;
   if (width >= 1400) maxChars = 130;
   return maxChars;
+};
+
+const requestData = async (searchString) => {
+  try {
+    const response = await fetch(searchString);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const processWikiResults = (results) => {
+  const resultArray = [];
+  Object.keys(results).forEach((key) => {
+    const id = key;
+    const title = results[key].title;
+    const text = results[key].extract;
+    const img = results[key].hasOwnProperty("thumvnail")
+      ? results[key].thumbnail.source
+      : null;
+    const item = {
+      id: id,
+      title: title,
+      img: img,
+      text: text,
+    };
+    resultArray.push;
+  });
+  return resultArray;
 };
