27afb883ab3d6903b41a93a39560fc371ef4ed12
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-01-25 18:39:54 -0600

    Created 2 consts rawSearchString and searchString

--- js/dataFunctions.js
+++ js/dataFunctions.js
@@ -11,6 +11,8 @@ export const retrieveSearchResults = async (searchTerm) => {
 
 const getWikiSearchString = (searchTerm) => {
   const maxChars = getMaxChars();
+  const rawSearchString = `https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=${searchTerm}&gsrlimit=20&prop=pageimages|extracts&exchars=${maxChars}&exintro&explaintext&exlimit=max&format=json&origin=*`;
+  const searchString = eccodeURI(rawSearchString);
 };
 
 const getMaxChars = () => {
